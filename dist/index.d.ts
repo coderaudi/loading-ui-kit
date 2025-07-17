aad94517cd25b6cbbcdeebebc54147ac0d317bed
abhijeet khire <abhijeet.khire@infobeans.com>
2025-07-17 17:13:15 +0530

    FIx : Import fix added

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+export interface LoadingProps {
+    loading: boolean;
+    textBelowLoadingCircle?: string;
+    logoInsideLoadingCircle?: React.ReactNode;
+}
+declare const Loading: React.FC<LoadingProps>;
+export default Loading;
